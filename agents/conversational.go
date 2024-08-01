aea33b5f5d03455ca9ef60bdc8f396be7fbdf871
chensilun <chensilun@baidu.com>
2024-08-01 15:52:59 +0800

    add a line break to scratchpad

--- agents/conversational.go
+++ agents/conversational.go
@@ -122,7 +122,7 @@ func constructScratchPad(steps []schema.AgentStep) string {
 	if len(steps) > 0 {
 		for _, step := range steps {
 			scratchPad += "\n" + step.Action.Log
-			scratchPad += "\nObservation: " + step.Observation
+			scratchPad += "\nObservation: " + step.Observation + "\n"
 		}
 	}
 
